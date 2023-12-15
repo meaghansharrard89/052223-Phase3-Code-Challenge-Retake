125ef2ed7f24c4fd2c8d9ba3da8a92294762df13
Meaghan <meaghan.sharrard89@gmail.com>
2023-12-14 18:20:49 -0800

    passing tests

--- lib/classes/movie.py
+++ lib/classes/movie.py
@@ -1,18 +1,41 @@
 class Movie:
-    
+    all = []
+
     def __init__(self, title):
         self.title = title
+        Movie.all.append(self)
+
+    # Movie-Title getter and setter
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, title):
+        if isinstance(title, str) and len(title) > 0:
+            self._title = title
+        else:
+            raise Exception
 
     def reviews(self):
         from classes.review import Review
-        pass
-    
+
+        return [review for review in Review.all if review.movie == self]
+
     def reviewers(self):
-        pass
-    
+        return [review.viewer for review in self.reviews()]
+
     def average_rating(self):
-        pass
+        ratings = [review.rating for review in self.reviews()]
+        if ratings:
+            return sum(ratings) / len(ratings)
+        else:
+            return 0
 
     @classmethod
     def highest_rated(cls):
-        pass
+        if not cls.all:
+            return None
+
+        highest_rated = max(cls.all, key=lambda movie: movie.average_rating())
+        return highest_rated
--- lib/classes/review.py
+++ lib/classes/review.py
@@ -1,9 +1,48 @@
 from classes.movie import Movie
 from classes.viewer import Viewer
 
+
 class Review:
-    
+    all = []
+
     def __init__(self, viewer, movie, rating):
         self.viewer = viewer
         self.movie = movie
         self.rating = rating
+        Review.all.append(self)
+
+    # Review-Rating getter and setter
+    @property
+    def rating(self):
+        return self._rating
+
+    @rating.setter
+    def rating(self, rating):
+        if isinstance(rating, int) and 1 <= rating <= 5:
+            self._rating = rating
+        else:
+            raise Exception
+
+    # Review-Viewer getter and setter
+    @property
+    def viewer(self):
+        return self._viewer
+
+    @viewer.setter
+    def viewer(self, viewer):
+        if isinstance(viewer, Viewer):
+            self._viewer = viewer
+        else:
+            raise Exception
+
+    # Review-Movie getter and setter
+    @property
+    def movie(self):
+        return self._movie
+
+    @movie.setter
+    def movie(self, movie):
+        if isinstance(movie, Movie):
+            self._movie = movie
+        else:
+            raise Exception
